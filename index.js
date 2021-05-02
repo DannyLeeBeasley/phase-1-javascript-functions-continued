324a502972bf26704ef671a54b9c27b7a0824cec
Dan Beasley <dannyleebeasley@gmail.com>
2021-05-01 20:49:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(saturdayActivity = "roller-skate"){
+    return `This Saturday, I want to ${saturdayActivity}!`
+} 
+
+function mondayWork(mondayActivity = "go to the office"){
+    return `This Monday, I will ${mondayActivity}.`
+}
+
+function wrapAdjective(flairString = "*"){
+    const innerFunction = function(someAdjective = "special"){
+        return `You are ${flairString}${someAdjective}${flairString}!`
+    }
+    return innerFunction
+}
