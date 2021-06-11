4be5e18716433f164bb9f5a082d2b44db4054d2e
Dan Beasley <dannyleebeasley@gmail.com>
2021-06-11 13:28:12 -0500

    second submission

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-function saturdayFun(saturdayActivity = "roller-skate"){
-    return `This Saturday, I want to ${saturdayActivity}!`
-} 
-
-function mondayWork(mondayActivity = "go to the office"){
-    return `This Monday, I will ${mondayActivity}.`
+const saturdayFun = (activity = 'roller-skate') => {
+    return `This Saturday, I want to ${activity}!`
 }
 
-function wrapAdjective(flairString = "*"){
-    const innerFunction = function(someAdjective = "special"){
-        return `You are ${flairString}${someAdjective}${flairString}!`
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair = '*'){
+    const innerFunction = function(adjective = 'special'){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
     }
     return innerFunction
 }
+
+wrapAdjective('~')('a goddamn genius')
